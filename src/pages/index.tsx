83bb5c16fe5ed2ca49460243e88c0019f5936c85
Tiago Santos <trodrigues322@gmail.com>
2022-03-05 09:47:29 -0300

    chore: commit standardization with (Commitlint, Husky, Lint-staged and Commitzen)

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Fake ATM</title>
+      </Head>
+      <main>
+        <h1>
+          Welcome to Fake-ATM
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
